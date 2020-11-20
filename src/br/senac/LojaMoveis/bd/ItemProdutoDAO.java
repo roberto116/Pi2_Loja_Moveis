fa59b6e5b66f15517cb0a84d6ebb1364c613d0e6
roberto <rrrobertodias@gmail.com>
2020-11-20 14:47:14 -0300

    foi programado o CRUD do produto

--- src/br/senac/LojaMoveis/bd/ItemProdutoDAO.java
+++ src/br/senac/LojaMoveis/bd/ItemProdutoDAO.java
@@ -5,10 +5,138 @@
  */
 package br.senac.LojaMoveis.bd;
 
+import br.senac.LojaMoveis.registros.ItemProduto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Beto
  */
 public class ItemProdutoDAO {
     
+    public static void inserir(ItemProduto item)throws Exception{
+        String sql = "INSERT INTO produto (produto, cor, qualidade, quantidade, preco) VALUES (?, ?, ?, ?, ?)";
+        
+        Connection conexao = ConnectionUtils.getConnection();
+        
+        try{
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            
+            comando.setString(1, item.produto);
+            comando.setString(2, item.cor);
+            comando.setString(3, item.qualidade);
+            comando.setInt(4, item.quantidade);
+            comando.setInt(5, item.preco);
+            comando.execute();
+            
+        }finally{
+            conexao.close();
+        }
+    }
+    
+    public static void excluir(int id) throws Exception{
+        String sql = "DELETE FROM produto WHERE id = ?";
+        
+        Connection conexao = ConnectionUtils.getConnection();
+        
+        try{
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            
+            comando.setInt(1, id);
+   
+            comando.execute();
+        }finally{
+            conexao.close();
+        }
+    }
+    
+    public static List<ItemProduto> listar() throws Exception{
+        String sql = "SELECT * FROM produto";
+        
+        Connection conexao = ConnectionUtils.getConnection();
+        List<ItemProduto> lista = new ArrayList();
+        
+        try{
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            
+            ResultSet dados = comando.executeQuery();
+            
+            while(dados.next()){
+                ItemProduto item = new ItemProduto();
+                
+                item.id = dados.getInt("id");
+                item.produto = dados.getString("produto");
+                item.cor = dados.getString("cor");
+                item.qualidade = dados.getString("qualidade");
+                item.quantidade = dados.getInt("quantidade");
+                item.preco = dados.getInt("preco");
+                lista.add(item); 
+            }
+            
+        }finally{
+            conexao.close();
+        }
+        
+        return lista;
+    }
+    
+    public static List<ItemProduto> pesquisar(String produto) throws Exception{
+        String sql = "SELECT * FROM produto WHERE produto LIKE ?";
+        
+        Connection conexao = ConnectionUtils.getConnection();
+        List<ItemProduto> lista = new ArrayList();
+        
+        try{
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            
+            comando.setString(1, "%" + produto + "%");
+            
+            ResultSet dados = comando.executeQuery();
+            
+            while(dados.next()){
+                ItemProduto item = new ItemProduto();
+                
+                item.id = dados.getInt("id");
+                item.produto = dados.getString("produto");
+                item.cor = dados.getString("cor");
+                item.qualidade = dados.getString("qualidade");
+                item.quantidade = dados.getInt("quantidade");
+                item.preco = dados.getInt("preco");
+                
+                lista.add(item);
+                
+            }
+            
+        }finally{
+            conexao.close();
+        }
+        
+        return lista;
+    }
+    
+    public static void editar(ItemProduto item) throws Exception{
+        String sql = "UPDATE produto SET produto = ?, cor = ?, qualidade = ?, quantidade = ?, preco = ? WHERE id = ?"; 
+        
+        Connection conexao = ConnectionUtils.getConnection();
+        
+        try{
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            
+            comando.setString(1, item.produto);
+            comando.setString(2, item.cor);
+            comando.setString(3, item.qualidade);
+            comando.setInt(4, item.quantidade);
+            comando.setInt(5, item.preco);
+            comando.setInt(6, item.id);
+            
+            comando.execute();
+            
+        }finally{
+            conexao.close();
+        }
+    }
 }
--- src/br/senac/LojaMoveis/telas/TelaProdutoController.java
+++ src/br/senac/LojaMoveis/telas/TelaProdutoController.java
@@ -5,12 +5,19 @@
  */
 package br.senac.LojaMoveis.telas;
 
-import br.senac.LojaMoveis.registros.itemProduto;
+import br.senac.LojaMoveis.bd.ItemProdutoDAO;
+import br.senac.LojaMoveis.registros.ItemProduto;
 import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -36,25 +43,32 @@ public class TelaProdutoController implements Initializable {
     @FXML
     private TextField tfPesquisar;
     @FXML
-    private TableView<itemProduto> tabelaProduto;
+    private TableView<ItemProduto> tabelaProduto;
     @FXML
-    private TableColumn<itemProduto, String> colunaProduto;
+    private TableColumn<ItemProduto, String> colunaProduto;
     @FXML
-    private TableColumn<itemProduto, String> colunaCor;
+    private TableColumn<ItemProduto, String> colunaCor;
     @FXML
-    private TableColumn<itemProduto, String> colunaQualidade;
+    private TableColumn<ItemProduto, String> colunaQualidade;
     @FXML
-    private TableColumn<itemProduto, ?> colunaQuantidade;
+    private TableColumn<ItemProduto, Integer> colunaQuantidade;
     @FXML
-    private TableColumn<itemProduto, ?> colunaValor;
+    private TableColumn<ItemProduto, Integer> colunaValor;
+    @FXML
+    private Button btnSalvar;
+    
+    //Variaveis globais
+    boolean editMode = false;
+    ItemProduto itemProdutoEdicao = null;
 
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         colunaProduto.setCellValueFactory(new PropertyValueFactory("produto"));
         colunaCor.setCellValueFactory(new PropertyValueFactory("cor"));
         colunaQualidade.setCellValueFactory(new PropertyValueFactory("qualidade"));
-        //colunaQuantidade.setCellValueFactory(new PropertyValueFactory("quantidade"));
-        //colunaValor.setCellValueFactory(new PropertyValueFactory("valor"));
+        colunaQuantidade.setCellValueFactory(new PropertyValueFactory("quantidade"));
+        colunaValor.setCellValueFactory(new PropertyValueFactory("preco"));
     }    
 
     @FXML
@@ -64,26 +78,151 @@ public class TelaProdutoController implements Initializable {
         tfQualidade.clear();
         tfQuantidade.clear();
         tfValor.clear();
+        
+        btnSalvar.setText("Inserir");
+        editMode = false;
     }
 
     @FXML
     private void inserir(ActionEvent event) {
+        if(!editMode){
+            ItemProduto item = new ItemProduto();
+        
+            item.produto = tfProduto.getText();
+            item.cor = tfCor.getText();
+            item.qualidade = tfQualidade.getText();
+            item.quantidade = Integer.parseInt(tfQuantidade.getText());
+            item.preco = Integer.parseInt(tfValor.getText());
+       
+        try{
+            ItemProdutoDAO.inserir(item);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Inserir");
+            alert.setHeaderText("Inserido com Sucesso");
+            alert.setContentText("Click em OK para continuar");
+            alert.showAndWait();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Inserir");
+            alert.setHeaderText("Valha ao Inserir");
+            alert.setContentText("Click em OK para continuar");
+            alert.showAndWait();
+        }
+            
+        }else{
+            itemProdutoEdicao.produto = tfProduto.getText();
+            itemProdutoEdicao.cor = tfCor.getText();
+            itemProdutoEdicao.qualidade = tfQualidade.getText();
+            itemProdutoEdicao.quantidade = Integer.parseInt(tfQuantidade.getText());
+            itemProdutoEdicao.preco = Integer.parseInt(tfValor.getText());
+            
+            try {
+                ItemProdutoDAO.editar(itemProdutoEdicao);
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Salvar");
+                alert.setHeaderText("Salvo com Sucesso");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Salvar");
+                alert.setHeaderText("Valha ao Salvar");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+            } 
+        }
+        limparCampos(event);
+        pesquisar(event);
         
     }
 
     @FXML
     private void pesquisar(ActionEvent event) {
-        
+        if(tfPesquisar.getText().equals("")){
+            try{
+                List<ItemProduto> resultado = ItemProdutoDAO.listar();
+
+                tabelaProduto.setItems(FXCollections.observableArrayList(resultado));
+                tabelaProduto.refresh();
+                
+            }catch(Exception e){
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Pesquisar");
+                alert.setHeaderText("Falha ao Pesquisar");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+            }
+        }
+        else {
+            try{
+                List<ItemProduto> resultado = ItemProdutoDAO.pesquisar(tfPesquisar.getText());
+
+                tabelaProduto.setItems(FXCollections.observableArrayList(resultado));
+                tabelaProduto.refresh();
+                
+            }catch(Exception e){
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Pesquisar");
+                alert.setHeaderText("Falha ao Pesquisa");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+            }  
+        }
     }
 
     @FXML
     private void editar(ActionEvent event) {
+        ItemProduto itemSelecionado = tabelaProduto.getSelectionModel().getSelectedItem();
         
+        if(itemSelecionado != null){
+            editMode = true;
+            
+            itemProdutoEdicao = itemSelecionado;
+            
+            tfProduto.setText(itemProdutoEdicao.produto);
+            tfCor.setText(itemProdutoEdicao.cor);
+            tfQualidade.setText(itemProdutoEdicao.qualidade);
+            tfQuantidade.setText(String.valueOf(itemProdutoEdicao.quantidade));
+            tfValor.setText(String.valueOf(itemProdutoEdicao.preco));            
+            
+            btnSalvar.setText("Salvar");
+        }
     }
 
     @FXML
     private void excluir(ActionEvent event) {
+        ItemProduto itemSelecionado = tabelaProduto.getSelectionModel().getSelectedItem();
         
+        if(itemSelecionado != null){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmar Remoção");
+            alert.setContentText("Remover o item " + itemSelecionado.produto);
+            
+            Optional<ButtonType> resultado = alert.showAndWait();
+                if(resultado.get() == ButtonType.OK){
+                    try {
+                    ItemProdutoDAO.excluir(itemSelecionado.id);
+                    alert.setTitle("Excluir");
+                    alert.setHeaderText("Excluido");
+                    alert.setContentText("Click em OK para continuar");
+                    alert.showAndWait();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    alert.setTitle("Excluir");
+                    alert.setHeaderText("Valha ao Excluir");
+                    alert.setContentText("Click em OK para continuar");
+                    alert.showAndWait();
+                } 
+            }
+            pesquisar(event);
+        }
     }
     
 }
