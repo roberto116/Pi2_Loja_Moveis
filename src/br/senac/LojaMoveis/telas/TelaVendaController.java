ed5b382470d2f3ab8c3572d17583dea23046a30c
Lenovo <Lenovo@DESKTOP-5GM8SP8>
2020-11-28 19:14:43 -0300

    Venda 95% feito

--- src/br/senac/LojaMoveis/telas/TelaVendaController.java
+++ src/br/senac/LojaMoveis/telas/TelaVendaController.java
@@ -4,15 +4,23 @@
  * and open the template in the editor.
  */
 package br.senac.LojaMoveis.telas;
+//import br.senac.LojaMoveis.bd.ItemProdutoDAO;
+//import br.senac.LojaMoveis.registros.Produto;
+//import br.senac.LojaMoveis.registros.Vendas;
+//import br.senac.LojaMoveis.registros.Vendas_Produtos;
 import br.senac.LojaMoveis.bd.ItemProdutoDAO;
 import br.senac.LojaMoveis.registros.Produto;
 import br.senac.LojaMoveis.registros.Vendas;
 import br.senac.LojaMoveis.registros.Vendas_Produtos;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -31,33 +39,29 @@ public class TelaVendaController implements Initializable {
     @FXML
     private Label tfValorTotal;
     @FXML
-    private TableView<?> tabelaproduto;
+    private TextField tfPesquisar;
     @FXML
-    private TableView<?> tabelacliente;
+    private TableView<Produto> tabelaProduto;
     @FXML
-    private TableColumn<?, ?> Colunacodigo;
+    private TableColumn<Produto, String> colunaProduto;
     @FXML
-    private TableColumn<?, ?> colunaproduto;
+    private TableColumn<Produto, String> colunaCor;
     @FXML
-    private TableColumn<?, ?> colunaestoque;
+    private TableColumn<Produto, String> colunaMarca;
     @FXML
-    private TableColumn<?, ?> colunavalor;
+    private TableColumn<Produto, Integer> colunaQuantidade;
     @FXML
-    private TableColumn<?, ?> colunacodigocliente;
-    @FXML
-    private TableColumn<?, ?> colunanome;
-    @FXML
-    private TableColumn<?, ?> colunacpf;
-
-    
+    private TableColumn<Produto, Integer> colunaValor;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-        Colunacodigo.setCellValueFactory(new PropertyValueFactory("id"));
-        //colunaCor.setCellValueFactory(new PropertyValueFactory("cor"));
-        //colunaMarca.setCellValueFactory(new PropertyValueFactory("marca"));
-        //colunaQuantidade.setCellValueFactory(new PropertyValueFactory("quantidade"));
-        //colunaValor.setCellValueFactory(new PropertyValueFactory("preco"));
+        
+         colunaProduto.setCellValueFactory(new PropertyValueFactory("produto"));
+        colunaCor.setCellValueFactory(new PropertyValueFactory("cor"));
+        colunaMarca.setCellValueFactory(new PropertyValueFactory("marca"));
+        colunaQuantidade.setCellValueFactory(new PropertyValueFactory("estoque"));
+        colunaValor.setCellValueFactory(new PropertyValueFactory("preco"));
+       
     }    
        
 
@@ -66,5 +70,95 @@ public class TelaVendaController implements Initializable {
         
         
     }
+
     
+    @FXML
+    private void pesquisar(ActionEvent event) {
+        if(tfPesquisar.getText().equals("")){
+            try{
+                List<Produto> resultado = ItemProdutoDAO.listar();
+
+                tabelaProduto.setItems(FXCollections.observableArrayList(resultado));
+                tabelaProduto.refresh();
+                
+            }catch(Exception e){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Pesquisar");
+                alert.setHeaderText("Falha ao Pesquisar");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+            }
+        }
+        else {
+            try{
+                List<Produto> resultado = ItemProdutoDAO.pesquisar(tfPesquisar.getText());
+
+                tabelaProduto.setItems(FXCollections.observableArrayList(resultado));
+                tabelaProduto.refresh();
+                
+            }catch(Exception e){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Pesquisar");
+                alert.setHeaderText("Falha ao Pesquisa");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+            }  
 }
+    }          
+
+    @FXML
+    private void calcular(ActionEvent event)
+    {
+        Produto itemSelecionado = tabelaProduto.getSelectionModel().getSelectedItem();
+        
+        
+        int valor = Integer.parseInt(tfQtd.getText());
+        
+       
+        if(valor < itemSelecionado.preco && valor > 0)
+        {
+            try{
+                double Calc = itemSelecionado.preco * valor;
+        
+                String CalcTotal = "" + Calc;
+        
+                tfValorTotal.setText(CalcTotal);
+            }
+            catch(Exception e){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Calcular");
+                alert.setHeaderText("Falha no Calculo,Tente Novamente");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+            }  
+        }
+        else
+        {
+            try{
+             Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Calcular");
+                alert.setHeaderText("Quantidade Invalida,Verifique o estoque");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+            }
+             catch(Exception e){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Calcular");
+                alert.setHeaderText("Falha no Calculo,Tente Novamente");
+                alert.setContentText("Click em OK para continuar");
+                alert.showAndWait();
+            }  
+        }
+        
+    }
+        
+        
+    
+       
+}
+    
+
+   
+    
+    
+
